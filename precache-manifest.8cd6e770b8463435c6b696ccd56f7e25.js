e2cd91ae58eaa3eb010387cbc652f94eda12d83d
sachin a kumbar <kumbarsachin67@gmail.com>
2020-09-25 00:23:12 +0530

    Updates

---
+++ precache-manifest.8cd6e770b8463435c6b696ccd56f7e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1969fb0467f390cc48b5cf8dd48303",
+    "url": "/SearchBar/index.html"
+  },
+  {
+    "revision": "f258aab8ef5373fed643",
+    "url": "/SearchBar/static/css/main.712df580.chunk.css"
+  },
+  {
+    "revision": "e85e8728c68bb04404b6",
+    "url": "/SearchBar/static/js/2.251ed680.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SearchBar/static/js/2.251ed680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f258aab8ef5373fed643",
+    "url": "/SearchBar/static/js/main.c9bff656.chunk.js"
+  },
+  {
+    "revision": "b8333fad21b38b29e217",
+    "url": "/SearchBar/static/js/runtime-main.119fc4fb.js"
+  }
+]);
